f0b7a6c5fe05c42d788538e7b93e88f00aa70d6a
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-04-25 13:50:12 +0530

    Update 284. Peeking Iterator.cpp

--- Low level design/284. Peeking Iterator.cpp
+++ Low level design/284. Peeking Iterator.cpp
@@ -1,11 +1,8 @@
 class PeekingIterator : public Iterator {
-        vector<int> v;
-        int i=0,n;
+        vector<int> v;int i=0,n;
 public:
-	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-        v=nums;     
-        n = v.size();
-	} 
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) 
+	{v=nums;n = v.size();} 
 	int peek(){return v[i];}
 	int next(){return v[i++];} 
 	bool hasNext() const {return i<n;}	    
