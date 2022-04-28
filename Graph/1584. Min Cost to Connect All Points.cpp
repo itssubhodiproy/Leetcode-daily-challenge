e4e79f7ed65412e20c46ea2b820b47f45af8b535
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-04-28 12:37:11 +0530

    Update 1584. Min Cost to Connect All Points.cpp

--- Graph/1584. Min Cost to Connect All Points.cpp
+++ Graph/1584. Min Cost to Connect All Points.cpp
@@ -1,36 +1,50 @@
 //if we use krushkal algo, we'll need to know the concept of Disjoint set union
 
+//rank compression
+int findPar(int n,vector<int>&parent){
+    if(n==parent[n]) return n;
+    return parent[n]=findPar(parent[n],parent);
+}
+void unionn(int u,int v, vector<int>&parent, vector<int>&rank){
+     u=findPar(u,parent);
+     v=findPar(v,parent);
+    if(rank[u]>=rank[v]){
+        parent[v]=u;
+        rank[u]++;
+    }
+    else{
+        parent[u]=v;
+        rank[v]++;
+    }
+}
+
 class Solution {
 public:
-    int find(vector<int> &ds, int i) {
-      return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
-}
-     int spanningTree(int n, vector<array<int, 3>> &arr)
-    {
-        int res=0;vector<int> ds(n, -1);
-        make_heap(begin(arr), end(arr), greater<array<int, 3>>());   
-    while (!arr.empty()) {
-        pop_heap(begin(arr), end(arr), greater<array<int, 3>>());
-        auto [dist, i, j] = arr.back();
-        arr.pop_back();
-        i = find(ds, i), j = find(ds, j);
-        if (i != j) {
-            res += dist;
-            ds[i] += ds[j];
-            ds[j] = i;
-            if (ds[i] == -n)
-                break;
-        }
-    }
-    return res;
-}
-      
     int minCostConnectPoints(vector<vector<int>>& ps) {
-    vector<array<int, 3>> arr;
-    for (auto i = 0; i < n; ++i)
-        for (auto j = i + 1; j < n; ++j) {
-            arr.push_back({abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1]), i, j});
+        int n=ps.size();
+                   // wt,u,v
+        vector<tuple<int,int,int>>v;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int wt=abs(ps[i][0]-ps[j][0])+abs(ps[i][1]-ps[j][1]);
+                v.push_back(make_tuple(wt,i,j));
+            }
         }
-        return spanningTree(ps.size(),arr);
+        
+        sort(v.begin(),v.end());
+        vector<int> parent(n),rank(n,0);
+	    for(int i = 0;i<n;i++) 
+	    parent[i] = i; 
+	
+	int cost = 0;
+	for(auto it : v) {
+        int ele1=findPar(get<1>(it),parent);
+        int ele2=findPar(get<2>(it),parent);
+	    if(ele1 != ele2) {
+	        cost += get<0>(it); 
+	        unionn(ele1,ele2,parent,rank); 
+	    }
+    }
+	return cost;
     }
 };
